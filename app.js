a372a739af87d6548930622bafd64f5386fbfafa
Кот <93428046+Kotarsis8413@users.noreply.github.com>
2024-05-21 12:24:59 +0300

    a.j>v2.2004

--- app.js
+++ app.js
@@ -5,24 +5,20 @@ const pages = {
     mainpage: (
         <div>
             <h2 className="center">Главная</h2>
-            <itempool>
+            <div className="itempool">
                 <div className="item">
-                    <div>
                         <img src="assets/imgs/dvushka.jpg" className="m22"></img>
                         <h3 className="m22">Двушка</h3>
                         <h2 className="m22">500₽</h2>
                         <button className="m22">В корзину</button>
-                    </div>
                 </div>
                 <div className="item">
-                    <div>
-                        <img src="https://rustlabs.com/img/items180/smg.thompson.png" className=""></img>
-                        <h3 className="">Томик</h3>
-                        <h2 className="">750₽</h2>
-                        <button className="">В корзину</button>
-                    </div>
+                        <img src="https://rustlabs.com/img/items180/smg.thompson.png" className="m22"></img>
+                        <h3 className="m22">Томик</h3>
+                        <h2 className="m22">750₽</h2>
+                        <button className="m22">В корзину</button>
                 </div>
-            </itempool>
+            </div>
         </div>
     )
 }
