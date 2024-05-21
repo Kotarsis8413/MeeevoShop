60c459df1ca2e284f8b22ff8827e5d48cedbb1de
Кот <93428046+Kotarsis8413@users.noreply.github.com>
2024-05-21 16:38:52 +0300

    a.j>v2.3001

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pages = {
             <h2 className="center">Главная</h2>
             <div className="itempool">
                 <div className="item">
-                        <img src="assets/imgs/dvushka.jpg" className="m22"/>
+                        <img src="assets/imgs/dvushka.png" className="m22"/>
                         <h3 className="m22">Двушка</h3>
                         <h2 className="m22">500₽</h2>
                         <button className="m22">В корзину</button>
