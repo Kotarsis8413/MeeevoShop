2cf5ead496ac9b8b98f62428e2e5f6cc58c9e5c2
Кот <93428046+Kotarsis8413@users.noreply.github.com>
2024-05-23 08:51:40 +0300

    a.j>v2.5; progress>99%

--- app.js
+++ app.js
@@ -9,27 +9,27 @@ const pages = {
                 <div className="item">
                         <img src="assets/imgs/dvushka.png" className="m22"/>
                         <h3 className="m22">Двушка</h3>
-                        <div className="m22"><h2>649₽</h2><button className="cartbtn">В корзину</button></div>
+                        <div className="m22"><h2>649₽</h2><button className="cartbtn" onClick={addToCart(1)}>В корзину</button></div>
                 </div>
                 <div className="item">
                         <img src="assets/imgs/tomik.png" className="m22"/>
                         <h3 className="m22">Томик</h3>
-                        <div className="m22"><h2>749₽</h2><button className="cartbtn">В корзину</button></div>
+                        <div className="m22"><h2>749₽</h2><button className="cartbtn" onClick={addToCart(2)}>В корзину</button></div>
                 </div>
                 <div className="item">
                         <img src="assets/imgs/kalash.png" className="m22"/>
                         <h3 className="m22">Калаш</h3>
-                        <div className="m22"><h2>999₽</h2><button className="cartbtn">В корзину</button></div>
+                        <div className="m22"><h2>999₽</h2><button className="cartbtn" onClick={addToCart(3)}>В корзину</button></div>
                 </div>
                 <div className="item">
                         <img src="assets/imgs/sochel.png" className="m22"/>
                         <h3 className="m22">Сочель</h3>
-                        <div className="m22"><h2>649₽</h2><button className="cartbtn">В корзину</button></div>
+                        <div className="m22"><h2>649₽</h2><button className="cartbtn" onClick={addToCart(4)}>В корзину</button></div>
                 </div>
                 <div className="item">
                         <img src="assets/imgs/sishka.png" className="m22"/>
                         <h3 className="m22">Сишка</h3>
-                        <div className="m22"><h2>949₽</h2><button className="cartbtn">В корзину</button></div>
+                        <div className="m22"><h2>949₽</h2><button className="cartbtn" onClick={addToCart(5)}>В корзину</button></div>
                 </div>
             </div>
         </div>
@@ -56,5 +56,18 @@ function changePage(page) {
         default:
             root.render(<h2>RENDER ERROR</h2>);
             break;
+    }
+}
+
+function addToCart(item) {
+    var name = prompt("Введите имя пользователя (если вы ещё не зарегистрировались перейдите в корзину)");
+    var pass = prompt("Введите ваш пароль");
+    alert("GitHub Pages не поддерживает серверные скрипты, поэтому весь backend перенесён на другой хостинг");
+
+    if (item != 1 || item != 2 ||item != 3 ||item != 4 ||item != 5) {
+
+    }
+    else {
+        window.location = `http://meeevogrup.temp.swtest.ru/MeeevoShop/cart/api?name=${name}&pass=${pass}&item=${item}`;
     }
 }
